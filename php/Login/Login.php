0033d1bedb1fa056efb9da814e700f0f8e6189c8
FJDeLaTorreUrea <ftorure1906@g.educaand.es>
2021-11-28 20:14:45 +0100

    Volvemos a v.1

--- php/Login/Login.php
+++ php/Login/Login.php
@@ -6,7 +6,8 @@
     if(Conexion::Login($usuario,$contrasena)==true)
     {
         echo"<script>alert('Usuario encontrado');</script>";
-        header("Location:../Pagina_principal/Pagina_principaal.html");
+        header("Location:../../html/Pagina_principal/Pagina_principaal.html");
+        
     }
 
 ?>
